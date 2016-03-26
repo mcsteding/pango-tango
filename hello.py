4c3bc05fa3004e51bf32d71a2c298e6cce0c3fdf
mcsteding <mcsteding@gmail.com>
2016-03-26 19:28:22 +0000

    updated hello.py

--- hello.py
+++ hello.py
@@ -1,13 +1,17 @@
 import os
 
-from flask import Flask
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-    
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return "User %s logged in " % request.form['username']
+    return render_template('login.html')
+        
+
 if __name__ == '__main__' :
     host = os.getenv('IP', '0.0.0.0')
     port = int(os.getenv('PORT', 5000))
+    app.debug = True
     app.run(host=host, port=port)
